7756fdc4ae326ccbc05e533b908d09d59d044dfd
jakareya76 <jakareya1306@gmail.com>
2024-10-24 11:22:14 +0600

    create banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,28 @@
+import bannerBg from "../assets/banner.jpeg";
+
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section
+      style={{ backgroundImage: `url(${bannerBg})` }}
+      className="w-full h-screen bg-cover -mt-28 relative"
+    >
+      <div className="absolute top-0 left-0 w-full h-full bg-[#000000c7] z-10"></div>
+
+      <div className="container mx-auto relative z-30">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h2 className="text-5xl font-bold text-white">Hello World</h2>
+          <p className="text-white max-w-2xl text-center mx-auto my-5">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt
+            mollitia explicabo quasi quis dolore! Illum, natus quos magni sed
+            possimus quaerat delectus aliquid exercitationem
+          </p>
+          <button className="px-8 py-4 text-white bg-blue-500 rounded-xl">
+            Learn More
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,16 +2,19 @@ import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="container mx-auto flex items-center justify-between">
+    <nav className="container mx-auto flex items-center justify-between z-50 relative py-8">
       <div className="flex-1">
-        <img src={logo} alt="logo" className="w-24" />
+        <img src={logo} alt="logo" className="w-28" />
       </div>
-      <div className="flex-1">
+      <div className="">
         <ul className="flex items-center justify-center gap-8 text-xl">
           {navItems.map((item, idx) => {
             return (
               <li key={idx}>
-                <a href={item.path} className="font-medium">
+                <a
+                  href={item.path}
+                  className="font-medium text-white hover:text-gray-300"
+                >
                   {item.name}
                 </a>
               </li>
